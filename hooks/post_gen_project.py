b5c6e78b4df902e9891b5ad963ff44b0c96d590b
Jake Trimble <jake@radix.bio>
2022-03-27 13:01:20 -0400

    Add lib

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,9 +3,11 @@ import os, re, sys
 package_regex = r'^(?:[a-zA-Z][_0-9a-zA-Z]*\.)*[a-zA-Z][_0-9a-zA-Z]*$'
 package_name = '{{cookiecutter.src_package}}'
 
-if not re.matches(package_regex, package_name):
+if len(package_name) == 0:
+    sys.exit(0)
+elif not re.match(package_regex, package_name):
     print("Error: %s is not a valid JVM package name" % package_name)
     sys.exit(1)
-
-path = 'src/main/scala' + package_name.replace('.', '/')
-os.makedirs(path)
+else:
+    path = 'src/main/scala/' + package_name.replace('.', '/')
+    os.makedirs(path)
